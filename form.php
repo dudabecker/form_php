08597cd0db21f5960f51baa12628cda7178151d9
Duda Becker <dudabeckerfoz@gmail.com>
2022-02-10 16:57:55 -0300

    Update form.php

--- form.php
+++ form.php
@@ -4,13 +4,7 @@ $user = addslashes($_POST['User']);
 $password = addslashes($_POST['Password']);
 
 if($user == "aluno" && $password == "1234"){
-    echo '<h3 style="color:gold; 
-          display:flex;
-          justify-content:center;
-          height:330px;
-          background-color:cadetblue;
-          padding: 165px;">'
-          ."Login realizado com sucesso!".'</h3>';
+    header('Location: ./vetor.html');
 }
 else
     echo "<script>alert('Usuário ou senha incorretos');location= './index.html';</script>";
